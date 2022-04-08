534eaaa2fd16ba4b98b8b4fb4bf5b4bc8ad1a4a2
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-04-08 17:21:12 +0900

    admin 커스터마이징

--- liongram/posts/admin.py
+++ liongram/posts/admin.py
@@ -1,3 +1,36 @@
+import imp
+from operator import mod
+from tabnanny import verbose
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 5
+    min_num = 3
+    max_num = 5
+    verbose_name = '댓글'
+    verbose_name_plural = '댓글'
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'content',
+                    'created_at', 'view_count', 'writer')
+    # 데이터 하나일 경우 , 꼭 넣어줘야한다
+    # list_editable = ('content',) -> 수정가능하도록
+    list_filter = ('created_at',)
+    search_fields = ('id',)
+    search_help_text = '게시판 번호, 작성자 검색이 가능합니다'
+    readonly_fields = ('created_at',)
+    inlines = [CommentInline]
+
+    actions = ['make_published']
+
+    def make_published(modeladmin, request, queryset):
+        for item in queryset:
+            item.content = '운영 규정 위반으로 인한 게시글 삭제 처리'
+            item.save()
+
+# admin.site.register(Comment)
--- liongram/posts/models.py
+++ liongram/posts/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField(verbose_name='작성일')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
     post = models.ForeignKey(to='Post', on_delete=models.CASCADE)
     writer = models.ForeignKey(
         to=User, on_delete=models.CASCADE, null=True, blank=True)
