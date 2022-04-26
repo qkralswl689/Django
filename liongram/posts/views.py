f9d41fbfc1901a473450b416d9f1a8e313b4f7ac
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-04-26 16:32:56 +0900

    function based view 만들기 1

--- liongram/posts/views.py
+++ liongram/posts/views.py
@@ -1,3 +1,14 @@
+import re
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+
+def url_view(request):
+    data = {'code': '001', 'msg': '0k'}
+    # return HttpResponse('url_view')
+    return JsonResponse(data)
+
+
+def url_parameter_view(request, username):
+    return HttpResponse(username)
